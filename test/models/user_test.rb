2278a49c20a5d33df113a824798345d6c06ef583
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-04 12:47:01 -0500

    Adding 2 instances in fixtures for user test, and add two Failed tests for uniqueness of nickname and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,6 +16,19 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "uniqueness of nickname" do
-
+    @david2 = User.new(nickname: "mrDavid",
+                       email: "david2@example.com",
+                       password: "password",
+                       activated: true)
+    assert_not @david2.valid?
   end
+
+  test "uniqueness of email" do
+    @david2 = User.new(nickname: "mrDavid2",
+                       email: "david@example.com",
+                       password: "password",
+                       activated: true)
+    assert_not @david2.valid?
+  end
+
 end
