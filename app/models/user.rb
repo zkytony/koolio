437be7890251b369deac0a8ec64091bb3c091e01
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-04 12:13:02 -0500

    Fields created for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :nickname,   type: String
+  field :first_name, type: String
+  field :last_name,  type: String
+  field :email,      type: String
+  field :password,   type: String
+  field :birthday,   type: Date
+  field :male,       type: Boolean
+  field :activated,  type: Boolean
+  field :_id,        type: String, default: -> { nickname } # Custom id
+
 end
