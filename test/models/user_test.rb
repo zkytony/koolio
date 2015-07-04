c969d3cf356518061b6d1df7c203a0388e3f1a88
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-04 12:37:05 -0500

    Testing presence of important fields in User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,4 +2,20 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
+  test "presence of attributes - nickname, email, password, activated" do
+    @bob = User.new
+    assert_not @bob.valid?
+    @bob.nickname = "littlebob"
+    assert_not @bob.valid?
+    @bob.email = "bob@example.com"
+    assert_not @bob.valid?
+    @bob.password = "password"
+    assert_not @bob.valid?
+    @bob.activated = true
+    assert @bob.valid?
+  end
+
+  test "uniqueness of nickname" do
+
+  end
 end
