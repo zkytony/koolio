6c48d90951d376fc3724869228f08fd0e22e51be
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-09 20:09:55 -0500

    Elegant and fun testing code after using rspec with shoulda-matchers and mongoid-rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,38 +2,12 @@ require "rails_helper"
 require "spec_helper"
 
 RSpec.describe User, :type => :model do
-  it "validates that username must be present" do
-    david = User.new(email: "david@example.com",
-                     password: "123456",
-                     activated: true)
-    expect(david.valid?).to be(false)
-    david.username = "david"
-    expect(david.valid?).to be(true)
-  end
+  it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to validate_uniqueness_of(:username) }
 
-  it "validates that email must be present" do
-    david = User.new(username: "david",
-                     password: "123456",
-                     activated: true)
-    expect(david.valid?).to be(false)
-    david.email = "david@example.com"
-    expect(david.valid?).to be(true)
-  end
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
 
-  it "validates that password must be present" do
-    david = User.new(username: "david",
-                     email: "david@example.com",
-                     activated: true)
-    expect(david.valid?).to be(false)
-    david.password = "123456"
-    expect(david.valid?).to be(true)
-  end
-
-  it "validates that activated has default value false" do
-    david = User.new(username: "david",
-                     email: "david@example.com",
-                     password: "123456")
-    expect(david.valid?).to be(true)
-    expect(david.activated).to be(false)
-  end
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:activated) }
 end
