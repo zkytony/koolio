3c987c78c48b7e7cf40b7f3cec87343df6514f82
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-11 11:18:10 -0500

    Testing for secure password - all passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe User, :type => :model do
   it { is_expected.to validate_uniqueness_of(:email) }
 
   it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_length_of(:password).greater_than(6) } # greater_than(6) is minimum of 6
+
   it { is_expected.to validate_presence_of(:activated) }
+
+  it "should have non-empty password" do 
+    user = User.new(username: "user1", email: "user1@example.com",
+                    password: " " * 6, password_confirmation: " " * 6)
+    expect(user.valid?).to be false
+  end
 end
