47eed962202852c4717b7d485c3547beb52dc6cd
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-18 22:46:42 -0500

    Adding fields for Comment and Tag models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,14 @@
 class Comment
   include Mongoid::Document
+  incldue Mongoid::Timestamp
+
+  field :user_id,      type: String
+  field :card_id,      type: String
+  field :content,      type: Text
+  field :likes,        type: Integer
+
+  validates :user_id, presence: true
+  validates :card_id, presence: true
+  validates :content, presence: true
+
 end
