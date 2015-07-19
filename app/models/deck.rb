9a64ff8f14b8037327f781a1a442df346ae0eaea
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-18 22:37:13 -0500

    Adding basic fields for Deck model

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,13 @@
 class Deck
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title,        type: String
+  field :description,  type: String
+  field :cards,        type: Array, default: []
+  field :tags,         type: Array, default: []
+
+  validates :title, presence: true,
+                    length: { maximum: 255 }
+
 end
