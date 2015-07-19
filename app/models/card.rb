0100891cfef07327eeb21e7cf9abcf3be501325e
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-18 22:41:22 -0500

    Adding basic fields for Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,15 @@
 class Card
   include Mongoid::Document
+  incldue Mongoid::Timestamp
+
+  field :deck_id,       type: String
+  field :user_id,       type: String
+  field :front_content, type: Text
+  field :back_content,  type: Text
+  field :flips,         type: Integer, default: 0
+  field :comments,      type: Array,   default: []
+
+  validates :deck_id, presence: true
+  validates :user_id, presence: true
+  
 end
