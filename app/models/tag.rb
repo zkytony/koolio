ac6a8b5f89ac7d722e0206d6022a4ab68c6df6c8
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-18 22:46:42 -0500

    Adding fields for Comment and Tag models

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,10 @@
 class Tag
   include Mongoid::Document
+
+  field :name,       type: String
+  field :created_at, type: DateTime
+  field :_id,        type: String, default: -> { name }
+  
+  validates :name, presence: true
+
 end
