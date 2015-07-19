ef31ed2d7741de109cb8e3fcdd6867293a293c85
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-19 11:41:26 -0500

    Rspec tests passed for basic validation & association for all current models

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Deck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to validate_presence_of(:user_id) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_length_of(:title).less_than(255) }
+
+  it { is_expected.to belong_to(:user).of_type(User) }
+  it { is_expected.to have_many(:cards).with_foreign_key(:deck_id) }
+  it { is_expected.to have_many(:cards).with_dependent(:delete) }
+
 end
--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Following, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to validate_presence_of(:username) }  
+  it { is_expected.to be_embedded_in(:user) }
+
 end
