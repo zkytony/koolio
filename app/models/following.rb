4df6e82ac5f21621da143f7bbd6053ff037ee7a6
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-18 21:54:47 -0500

    Adding new model - Following

---
+++ app/models/following.rb
@@ -0,0 +1,10 @@
+class Following
+  include Mongoid::Document
+  
+  field :username,    type: String
+  field :created_at,  type: Date
+
+  validates :username, presence: true,
+
+  embedded_in :user
+end
