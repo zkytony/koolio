1f39496962bc92dc87dd1113bc3a7478e3f08c41
Kaiyu Zheng <kaiyutony@gmail.com>
2015-07-21 23:34:25 -0500

    Changing the modeling of User and Following. Have Following to be a separate model. No relation between User and Following at DB level

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Following, type: :model do
 
-  it { is_expected.to validate_presence_of(:username) }  
-  it { is_expected.to be_embedded_in(:user) }
-
+  it { is_expected.to validate_presence_of(:followee) }  
+  it { is_expected.to validate_presence_of(:follower) }
 end
