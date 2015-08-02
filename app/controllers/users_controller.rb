f8646256f40b46f10eaab6c350d72c957c83b36d
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-01 22:59:46 -0500

    Users controller added new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,34 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+    render "static_pages/index"
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      # redirect to users/:id
+      flash[:success] = "Welcome to Koolio"
+      log_in @user
+      redirect_to @user
+    else
+      # Render the form again; 
+      # Error will be displayed accordingly
+      flash[:error] = "Error in your field"
+      redirect_to root_path
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    
+    # strong parameter
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
