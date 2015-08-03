f97395e9e99ee91bdd65e271cd633a6922b144b3
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-02 22:46:11 -0500

    Remove the sql directory

--- app/models/sql/tag.rb
+++
@@ -1,9 +0,0 @@
-class Tag < ActiveRecord::Base
-
-  field :name,       type: String
-  field :created_at, type: DateTime
-  field :_id,        type: String, default: -> { name }
-  
-  validates :name, presence: true
-
-end
