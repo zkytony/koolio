0c61e2daeef7f38848ae714799dcbb5ffa46d7c5
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-03 22:02:54 -0500

    Decks migrated to pg.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,16 +1,9 @@
 class Deck < ActiveRecord::Base
-
-  field :user_id,      type: String
-  field :title,        type: String
-  field :description,  type: String
-  field :tags,         type: Array, default: []
-
   validates :user_id, presence: true
   validates :title, presence: true,
                     length: { maximum: 255 }
 
-  belongs_to :user,  dependent: :nullify
-  has_many :cards,   dependent: :delete
-  has_many :subscriptions, dependent: :delete
-
+  belongs_to :user
+  has_many :cards,   dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 end
