bc610815588a76ef73db0b880e9783ecdb96b54f
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-08 10:38:31 -0500

    User is able to follow each other. Basic tests passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
 
   has_many :decks,    dependent: :destroy
   has_many :cards,    dependent: :destroy
-  has_many :active_relationships, class_name: "Following", foreign_key: "followee_id", dependent: :destroy   # Follow others
-  has_many :following, through: :active_relationships, source: :followee
-  has_many :passive_relationships, class_name: "Following", foreign_key: "follower_id", dependent: :destroy  # Being followed by others
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy   # Follow others
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy  # Being followed by others
   has_many :followers, through: :passive_relationships
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,11 +21,11 @@ class User < ActiveRecord::Base
   has_secure_password   # Enforces validation on the virtual password & password_confirmation attributes
 
   def follow(other_user)
-    active_relationships.create(followee_id: other_user.id)
+    active_relationships.create(followed_id: other_user.id)
   end
 
   def unfollow(other_user)
-    active_relationships.find_by(followee_id: other_user.id).destroy
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
   def following?(other_user)
