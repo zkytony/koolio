ad324a4bb00d3bf6dc54a2e86b24362b2fa0d436
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-08 10:37:32 -0500

    Remove Following model & followings table, replace it by Relationship model. Migration included

--- spec/models/following_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Following, type: :model do
-  it { should validate_presence_of(:follower_id) }
-  it { should validate_presence_of(:followee_id) }
-  it { should belong_to(:follower).class_name("User") }
-  it { should belong_to(:followee).class_name("User") }
-end
