643ebe04d4682d95b3bf45a2218b677b68e1a1ac
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-09 19:47:55 -0500

    Because .card may appear in a lot of places, add the flip listener for .card in flip.js - included in all views

--- app/assets/javascripts/flip.js
+++ app/assets/javascripts/flip.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+    $(".card").click(function() {
+	flip($(this));
+    });
+});
+
 function flip(jqueryObj) {
   if (!jqueryObj.hasClass('flip')) {
     jqueryObj.addClass('flip');
