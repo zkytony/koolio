f41d288f57835515a7db30204d8bd4848751474d
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-09 19:07:04 -0500

    Able to get to the login or signup page from anywhere via the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
+  get 'signup' => 'users#new'
   resources :users
   resources :cards
   resources :decks
