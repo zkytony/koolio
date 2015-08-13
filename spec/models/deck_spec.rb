a6cbbaf912f794616fd5efe1b525ddf4fdb8c119
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-12 22:29:53 -0500

    After testing, the title for a deck should be idea for now if it has less than 175 chars

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Deck, type: :model do
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:title) }
-  it { should validate_length_of(:title).is_at_most(255) }
+  it { should validate_length_of(:title).is_at_most(175) }
 
   it { should belong_to(:user) }
   it { should have_many(:cards) }
