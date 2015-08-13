6980cf62461dbdb5e837d59b93322e727edc53f2
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-12 22:29:53 -0500

    After testing, the title for a deck should be idea for now if it has less than 175 chars

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,7 +1,7 @@
 class Deck < ActiveRecord::Base
   validates :user_id, presence: true
   validates :title, presence: true,
-                    length: { maximum: 255 }
+                    length: { maximum: 175 }
 
   belongs_to :user
   has_many :cards,   dependent: :destroy
