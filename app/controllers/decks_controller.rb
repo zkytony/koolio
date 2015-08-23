0b125c47f0fdcca391749ea6999247c91ac2de6f
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-22 19:19:16 -0500

    Dummy form for creating deck

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,7 +3,27 @@ class DecksController < ApplicationController
     @decks = Deck.all
   end
 
-  def show
-    # @deck = Deck.find(params[:id])
+  def new 
+    @deck = Deck.new
   end
+
+  def create
+    @deck = current_user.decks.build(deck_params)
+    if @deck.save!
+      redirect_to @deck
+    else
+      redirect_to new_deck_path
+    end
+  end
+
+  def show
+    @deck = Deck.find(params[:id])
+  end
+
+  private
+    
+    # strong parameter
+    def deck_params
+      params.require(:deck).permit(:title, :description, :user_id)
+    end
 end
