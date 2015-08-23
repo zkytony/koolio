44c83ef45a6f6eb3fbea06a104e6ca95f84fb9a0
Kaiyu Zheng <kaiyutony@gmail.com>
2015-08-22 19:22:28 -0500

    Able to craete card. Very primitively, using CKEditor

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,7 +7,26 @@ class CardsController < ApplicationController
     @card = Card.new
   end
 
+  def create
+    @deck = Deck.find(params[:deck_id])
+    @card = @deck.build_card(card_params, current_user)
+    if @card.save!
+      flash[:success] = "Craeted a new card"
+      redirect_to @deck
+    else
+      flash[:error] = "Unable to create card"
+      redirect_to new_dcek_card_path
+    end
+  end
+
   def show
     @card = Card.find(params[:id])
   end
+
+  private
+    
+    # strong parameter
+    def card_params
+      params.require(:card).permit(:front_content, :back_content, :deck_id, :user_id, :flips)
+    end
 end
