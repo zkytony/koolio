bd3a1cd1b109f0135ea5d3791fc38a7c67de10b4
Kaiyu Zheng <kaiyutony@gmail.com>
2015-09-06 14:57:01 -0500

    Added hide column to cards. Now cards can be hidden in a deck and not viewable by others. Tested

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Card, type: :model do
   it { should have_many(:comments).dependent(:destroy) }
   it { should have_many(:users_with_like).class_name("LikeCard").dependent(:destroy) }
   it { should have_many(:liked_users).through(:users_with_like).source(:user) }
+
+  it "should not be viewed once hidden" do
+    userA = User.create(username: "userA", email: "userA@example.com",
+                        password: "123456", password_confirmation: "123456")
+    userB = User.create(username: "userB", email: "userB@example.com",
+                        password: "123456", password_confirmation: "123456")
+    deck = userA.create_deck(title: "Testing deck", description: "Testing deck description")
+    card = deck.build_card({front_content: "Hi", back_content: "Bye"}, userA)
+    card.save!
+
+    # Deck is Open, so card should be viewable
+    expect(card.viewable_by?(userB)).to be true
+    card.update(hide: true)
+    # After hiding, card should not be viewable
+    expect(card.viewable_by?(userB)).to be false
+  end
 end
--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Deck, type: :model do
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:title) }
   it { should validate_length_of(:title).is_at_most(175) }
+  it { should validate_inclusion_of(:open).in_array([true, false]) }
 
   it { should belong_to(:user) }
   it { should have_many(:cards) }
