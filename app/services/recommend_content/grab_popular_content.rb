8db826a6f4c8f2bdc0fc0a96c60410d1d21ca4df
Kaiyu Zheng <kaiyutony@gmail.com>
2016-01-12 18:39:11 -0800

    randomize explore cards

--- app/services/recommend_content/grab_popular_content.rb
+++ app/services/recommend_content/grab_popular_content.rb
@@ -7,9 +7,9 @@ class RecommendContent
     def self.call(user, n_card, more, card_ids)
       contents = []
       if more
-        contents = Card.where("cards.id NOT IN (?)", card_ids).sort_by(&:likes).slice(0, n_card)
+        contents = Card.where("cards.id NOT IN (?)", card_ids).sort_by(&:likes).slice(0, n_card).shuffle
       else
-        contents = Card.all.sort_by(&:likes).reverse.slice(0, n_card)
+        contents = Card.all.sort_by(&:likes).reverse.slice(0, n_card).shuffle
       end
       contents
     end
