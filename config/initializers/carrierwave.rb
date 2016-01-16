d6c8c89f11f48c7106db283eb52a7cb48cceea52
Kaiyu Zheng <kaiyutony@gmail.com>
2016-01-16 15:03:52 -0800

    only load aws config if storage type is fog.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,14 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        
-  config.fog_credentials = {
-    provider:              'AWS',     
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'], 
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                'us-west-2'
-  }
-  config.fog_directory  = ENV['AWS_S3_BUCKET']
+if ENV['STORAGE_TYPE'] == 'fog'
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        
+    config.fog_credentials = {
+      provider:              'AWS',     
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'], 
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                'us-west-2'
+    }
+    config.fog_directory  = ENV['AWS_S3_BUCKET']
+  end
 end
 
 module CarrierWave
