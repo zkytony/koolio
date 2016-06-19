3c9b1c67915190f5979e7ebb3f956d111750a17e
Kaiyu Zheng <kaiyutony@gmail.com>
2016-06-19 12:44:17 -0700

    Make link clickable. Though fires twice due to tap event

--- app/assets/javascripts/homepage_cards_handler.js
+++ app/assets/javascripts/homepage_cards_handler.js
@@ -9,8 +9,10 @@ CardsHandler.prototype.init = function() {
   var handler = this;
   // flip when clicked home; support mobile
   $(document).on("click tap", ".home-card", function(e) {
-    e.stopPropagation();
-    e.preventDefault();
+    if (e.target.nodeName != "A") {
+      e.stopPropagation();
+      e.preventDefault();
+    }
     if (!$(e.target).hasClass("no-flip")) {
       handler.handleFlip($(this));
     }
