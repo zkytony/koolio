1292d3392993130bcd322f5c83210e74384d2128
Kaiyu Zheng <kaiyutony@gmail.com>
2016-07-07 23:58:43 -0700

    Fixed js bug in flash alert and updated .gitignore

--- app/assets/javascripts/flash_alerts.js
+++ app/assets/javascripts/flash_alerts.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
 
 // type can be error, warning, info, or success
 // To have an alert show infinite amount of time, set wait to -1
-function addAlert(type, message, wait, id="") {
+function addAlert(type, message, wait, id) {
     var htmlStr = "";
     var extraClass = "";
     if (wait == -1) {
 	extraClass = "no-fade";
     }
-    if (id.length > 0) {
+    if (id != undefined) {
 	htmlStr = "<div id=\"" + id + "\"class=\"fl-alert alert-" + type + " " + extraClass + "\">" + message + "</div>";
     } else {
 	htmlStr = "<div class=\"fl-alert alert-" + type + " " + extraClass + "\">" + message + "</div>";
