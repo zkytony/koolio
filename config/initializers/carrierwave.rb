e25accb85f9a47565b7d5d184966ce5eaff5cd71
Kaiyu Zheng <kaiyutony@gmail.com>
2018-07-15 17:04:30 +0800

    rename ENV var for aws s3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ if ENV['STORAGE_TYPE'] == 'fog'
       provider:              'AWS',     
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'], 
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                ENV['AWS_REGION']
+      region:                ENV['AWS_S3_BUCKET_REGION']
     }
     config.fog_directory  = ENV['AWS_S3_BUCKET']
   end
