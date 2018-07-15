1badc6611da8b613c1ab50dd3a2b6c355dbe0381
Kaiyu Zheng <kaiyutony@gmail.com>
2018-07-15 17:01:37 +0800

    Need to specify region for carrierwave in ENV as well

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ if ENV['STORAGE_TYPE'] == 'fog'
       provider:              'AWS',     
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'], 
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                'us-west-2'
+      region:                ENV['AWS_REGION']
     }
     config.fog_directory  = ENV['AWS_S3_BUCKET']
   end
