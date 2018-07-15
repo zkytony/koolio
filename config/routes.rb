0ff60562ba194b96e2a078d68f0d8632a5d7764b
Kaiyu Zheng <kaiyutony@gmail.com>
2018-07-15 20:16:27 +0800

    Use signup page as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
 
-  root 'users#explore'
+  root 'users#new'
   
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
